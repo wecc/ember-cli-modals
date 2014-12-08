6b1446a0c1c7c196bc00e6ccab4c916301a21048
Christoffer Persson <christoffer@valdoo.se>
2014-12-08 21:28:05 +0100

    Upgraded to ember-cli 0.1.4

--- Brocfile.js
+++ Brocfile.js
@@ -17,7 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/bootstrap/dist/css/bootstrap.css');
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 module.exports = app.toTree();
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    modulePrefix: 'ember-cli-modals',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
@@ -27,7 +28,15 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
 
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
